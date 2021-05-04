d452912af8e0ec014cc3738d5599988cc6c0265a
Mansi More <45144625+MansiSMore@users.noreply.github.com>
2021-05-05 00:24:56 +0530

    Create START01.cpp

---
+++ START01.cpp
@@ -0,0 +1,28 @@
+/*
+Problem Statement
+Write a program that accepts a number, n, and outputs the same.
+
+Input
+The only line contains a single integer.
+
+Output
+Output the answer in a single line.
+
+Constraints
+0 ≤ n ≤ 105
+
+Sample Input :
+123
+Sample Output :
+123
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T = 0;
+    cin >> T;
+    cout << T;
+	return 0;
+}
