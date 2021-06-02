73c974caec6eb4eb116361376b30f4d4ac435708
Mansi More <45144625+MansiSMore@users.noreply.github.com>
2021-06-02 12:01:58 +0530

    Create TEST.cpp

---
+++ TEST.cpp
@@ -0,0 +1,34 @@
+/*
+Your program is to use the brute-force approach in order to find the Answer to Life, the Universe, and Everything. 
+More precisely… rewrite small numbers from input to output. 
+Stop processing input after reading in the number 42. 
+All numbers at input are integers of one or two digits.
+
+Sample Input:
+1
+2
+88
+42
+99
+
+Sample Output:
+1
+2
+88
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    while(!cin.eof())
+    {
+        int num = 0;
+        cin >> num;
+        if(num == 42)
+            break;
+        else
+            cout << num << endl;
+    }
+	return 0;
+}
